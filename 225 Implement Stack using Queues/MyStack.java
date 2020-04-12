505f87ece37386eece4b1c373524101c59c71ef3
richinn1020 <56169705+richinn1020@users.noreply.github.com>
2020-04-12 12:03:25 -0700

    Create MyStack.java

---
+++ 225 Implement Stack using Queues/MyStack.java
@@ -0,0 +1,43 @@
+// Approach #3 (One Queue, push - O(n)O(n), pop O(1)O(1) )
+
+class MyStack {
+    private Queue<Integer> q1;
+    /** Initialize your data structure here. */
+    public MyStack() {
+        q1 = new LinkedList<>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        q1.add(x);
+        int sz = q1.size();
+        while (sz > 1) {
+            q1.add(q1.remove());
+            sz--;
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return q1.remove();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return q1.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
